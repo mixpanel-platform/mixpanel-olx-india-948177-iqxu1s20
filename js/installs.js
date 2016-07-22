cedb1c20d3db16c59e0860c78f45fba32afa96c4
Bo <bowen.johnson@mixpanel.com>
2016-07-22 10:49:10 -0400

    Initial Commit

---
+++ js/installs.js
@@ -0,0 +1,22 @@
+var paramsToday = {
+    from: moment().subtract(30,'days'),    // get today's date
+    to: moment(),      // get today's date
+    type: 'unique',    // analysis type for the data, can be 'general', 'unique', or 'average'
+    unit: 'day',       // level of granularity of the data, can be 'minute', 'hour', 'day', or 'month'
+};
+//run query to get installs data for the last month
+MP.api.segment('App_install', paramsToday).done(function(installsResults) {
+    console.log("installs today results", installsResults.values());
+    //store results of install data for the last month
+    var installs = installsResults.values()
+    // get just today's installs for header panel
+    var todaysInstalls = addCommas(installs.App_install[paramsToday.to.format("YYYY-MM-DD")])
+    // replace text in header pannel to show today's installs
+    $('#installs-today').text(todaysInstalls)
+
+    //graph monthly data using MPChart
+    var newInstalls = $('#new-installs').MPChart({chartType: 'line'});   //create the chart and attach to the new installs dive                             
+    newInstalls.MPChart('setData', installsResults.values()); // Set the chart's data
+    $("#new-installs").show()           //display chart header
+
+});
